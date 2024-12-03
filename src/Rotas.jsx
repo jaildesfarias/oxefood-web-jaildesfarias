e3b53a1a7c729861ad22e5f3e317a828ece569f3
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-03 19:53:09 -0300

    Update Rotas.jsx

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -7,33 +7,31 @@ import ListCliente from './views/cliente/ListCliente';
 import FormEntregador from './views/entregador/FormEntregador';
 import ListEntregador from './views/entregador/ListEntregador';
 
-import FormProduto from './views/produto/FormProduto'; // Corrigido duplicação de importação
-import ListProduto from './views/produto/ListProduto'; // Adicionado o import para ListProduto
+import FormProduto from './views/produto/FormProduto';
+import ListProduto from './views/produto/ListProduto';
+
+import VendaList from './views/venda/VendaList';
 
 import Home from './views/home/Home';
 
 function Rotas() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="list-cliente" element={ <ListCliente/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
+        <Routes>
+           
+            <Route path="/" element={<Home />} />
 
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="list-produto" element={ <ListProduto/> } /> // Corrigido a rota para ListProduto 
+            <Route path="list-cliente" element={<ListCliente />} />
+            <Route path="form-cliente" element={<FormCliente />} />
 
-                <Route path="form-entregador" element={ <FormEntregador/> } />
+            <Route path="form-produto" element={<FormProduto />} />
+            <Route path="list-produto" element={<ListProduto />} />
 
-                <Route path="list-entregador" element={ <ListEntregador/> } />
+            <Route path="form-entregador" element={<FormEntregador />} />
+            <Route path="list-entregador" element={<ListEntregador />} />
 
-            // Corrigido os erros de digitação nas rotas 
-                <Route path="form-list-cliente" element={ <FormCliente/> } />
-                <Route path="form-list-produto" element={ <FormProduto/> } />
-                <Route path="form-list-entregador" element={ <FormEntregador/> } />
-                
-            </Routes>
-        </>
+             <Route path="form-vendas" element={<FormVendas />} />
+            <Route path="/vendas" element={<VendaList />} />
+        </Routes>
     );
 }
 
