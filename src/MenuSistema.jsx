fe4782bb2f67d78a8b869a56139ac2e3d23dbe29
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-12 15:17:34 -0300

    Update MenuSistema.jsx
    vendas

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -3,61 +3,72 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import MenuSistema from '../../MenuSistema';
 
-export default function MenuSistema (props) {
-
-   return(
+export default function MenuSistema(props) {
+   return (
        <div>
-          <MenuSistema tela={'cliente'} />
+           <MenuSistema tela={props.tela} />
 
            <Menu inverted>
-              
                <Menu.Item
-                   content='Home'
-                   active={props.tela === 'home'}
+                   content="Home"
+                   active={props.tela === "home"}
                    as={Link}
-                   to='/'
+                   to="/"
                />
 
                <Menu.Item
-                   content='Cliente'
-                   active={props.tela === 'cliente'}
+                   content="Cliente"
+                   active={props.tela === "cliente"}
                    as={Link}
-                   to='/form-cliente'
-               />
-              <Menu.Item
-                  content="Lista Cliente"
-                  active={props.tela === 'list-cliente'}
-                  as={Link}
-                  to="/list-cliente"
-               />
-              <Menu.Item
-                   content='Produto'
-                   active={props.tela === 'produto'}
-                   as={Link}
-                   to='/form-produto'
+                   to="/form-cliente"
                />
                <Menu.Item
-                  content="Lista Produto"
-                  active={props.tela === 'list-produto'}
-                  as={Link}
-                  to="/list-produto"
-                  />
-               <Menu.Item
-                   content='Entregador'
-                   active={props.tela === 'entregador'}
+                   content="Lista Cliente"
+                   active={props.tela === "list-cliente"}
                    as={Link}
-                   to='/form-entregador'
+                   to="/list-cliente"
                />
-              <Menu.Item
-                 content='Entregador'
-                 active={props.tela ==='List entregador'}
-                 as={Link}
-                 to='/List-entregador'
-                 />
-
+               
+               <Menu.Item
+                   content="Produto"
+                   active={props.tela === "produto"}
+                   as={Link}
+                   to="/form-produto"
+               />
+               <Menu.Item
+                   content="Lista Produto"
+                   active={props.tela === "list-produto"}
+                   as={Link}
+                   to="/list-produto"
+               />
+               
+               <Menu.Item
+                   content="Entregador"
+                   active={props.tela === "entregador"}
+                   as={Link}
+                   to="/form-entregador"
+               />
+               <Menu.Item
+                   content="Lista Entregador"
+                   active={props.tela === "list-entregador"}
+                   as={Link}
+                   to="/list-entregador"
+               />
+               
+               {/* Adicionando Menu de Vendas */}
+               <Menu.Item
+                   content="Venda"
+                   active={props.tela === "venda"}
+                   as={Link}
+                   to="/form-vendas"
+               />
+               <Menu.Item
+                   content="Lista Vendas"
+                   active={props.tela === "list-vendas"}
+                   as={Link}
+                   to="/vendas"
+               />
            </Menu>
        </div>
-   )
+   );
 }
-
-
